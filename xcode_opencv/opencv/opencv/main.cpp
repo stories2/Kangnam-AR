ac7e9abdc233e5f34c7889f7855f5cabf225f377
Kim HyunWoo <stories2@naver.com>
2021-03-04 23:23:20 +0900

    xcode c++

---
+++ xcode_opencv/opencv/opencv/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  opencv
+//
+//  Created by 김현우 on 2021/03/04.
+//
+
+#include <iostream>
+//https://www.vbflash.net/83
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    cout << "Open CV Version" << CV_VERSION << endl;
+    return 0;
+}

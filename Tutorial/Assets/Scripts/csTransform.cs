2086e1abde24ac3e7e036310a1912af5ddbf58fb
Kim HyunWoo <stories2@naver.com>
2021-03-10 01:20:09 +0900

    vscode plugin, move obj

---
+++ Tutorial/Assets/Scripts/csTransform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csTransform : MonoBehaviour
+{
+    float speed = 20.0f;
+    float rotSpeed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float mouseX = Input.GetAxis("Mouse X");
+
+        h = h * speed * Time.deltaTime;
+        v = v * speed * Time.deltaTime;
+        mouseX = mouseX * rotSpeed;
+
+        transform.Translate(Vector3.right * h);
+        transform.Translate(Vector3.forward * v);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}

c6ba85f1f898cd5dbe6ff85e1387770bdc279635
Kim HyunWoo <stories2@naver.com>
2021-04-10 16:25:43 +0900

    anim

---
+++ Tutorial/Assets/Scripts/csAnimation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csAnimation : MonoBehaviour
+{
+    bool state = false;
+    private void OnMouseDown() {
+        if (state)
+            GetComponent<Animator>().SetTrigger("Run");
+        else
+            GetComponent<Animator>().SetTrigger("Idle");
+        state = !state;
+    }
+}

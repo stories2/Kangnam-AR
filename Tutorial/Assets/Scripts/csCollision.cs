f09f7af6970cbdba71e47cc9498696bd29d68a2b
Kim HyunWoo <stories2@naver.com>
2021-05-10 16:21:44 +0900

    adf

--- Tutorial/Assets/Scripts/csCollision.cs
+++ Tutorial/Assets/Scripts/csCollision.cs
@@ -11,4 +11,31 @@ public class csCollision : MonoBehaviour
     private void OnTriggerExit(Collider other) {
         GetComponent<Animator>().SetTrigger("Idle");
     }
+
+    /// <summary>
+    /// OnMouseDown is called when the user has pressed the mouse button while
+    /// over the GUIElement or Collider.
+    /// </summary>
+    int count = 0;
+    void OnMouseDown()
+    {
+        switch(count) {
+            case 0:
+                GetComponent<Animator>().SetTrigger("run");
+            break;
+            case 1:
+                GetComponent<Animator>().SetTrigger("idle");
+            break;
+            case 2:
+                GetComponent<Animator>().SetTrigger("walk");
+            break;
+            case 3:
+                GetComponent<Animator>().SetTrigger("dance");
+            break;
+            case 4:
+                GetComponent<Animator>().SetTrigger("swing");
+            break;
+        }
+        count = (count + 1) % 5;
+    }
 }
---
+++ Tutorial/Assets/Scripts/csEvent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class csEvent : MonoBehaviour
+{
+    public AnchorBehaviour[] anchors;
+    
+    int index = 0;
+    bool flag = true;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            GetComponent<ContentPositioningBehaviour>().DuplicateStage = true;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            GetComponent<ContentPositioningBehaviour>().DuplicateStage = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GetComponent<ContentPositioningBehaviour>().AnchorStage = anchors[++index % anchors.Length];
+        }
+    }
+
+    public void HitTest(HitTestResult result) {
+        Debug.Log("HitTest");
+
+        if(flag) {
+            GetComponent<ContentPositioningBehaviour>().AnchorStage = anchors[0];
+        } else {
+            GetComponent<ContentPositioningBehaviour>().AnchorStage = anchors[1];
+        }
+
+        GetComponent<ContentPositioningBehaviour>().PositionContentAtPlaneAnchor(result);
+        flag = !flag;
+    }
+}
